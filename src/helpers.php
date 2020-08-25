cb2805cb396d2abeef466b2196adcca7236815bf
Yurij Karpov <acrossoffwest@gmail.com>
2020-08-25 15:36:36 +0200

    (fix) Bug with array_mapper function

--- src/helpers.php
+++ src/helpers.php
@@ -3,6 +3,6 @@
 if (!function_exists('array_mapper')) {
     function array_mapper (array $array)
     {
-        return \OnzaMe\Helpers\Helpers\ArrayMapper::create($array);
+        return \OnzaMe\Helpers\ArrayMapper::create($array);
     }
 }
