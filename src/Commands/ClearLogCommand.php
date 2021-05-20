df344fc810bda647ca8b067f13f526ef4b8c759b
LAPTOP-KFNK5QPH\uurus <moais611@gmail.com>
2021-05-20 14:48:33 +0700

    add clear log command

---
+++ src/Commands/ClearLogCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace OnzaMe\Helpers\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class ClearLogCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'logs:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear all logs';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Artisan::command('logs:clear', function() {
+
+            exec('rm -f ' . storage_path('logs/*.log'));
+
+            exec('rm -f ' . base_path('*.log'));
+
+            $this->comment('Logs have been cleared!');
+
+        })->describe('Clear log files');
+
+        return 0;
+    }
+}
+
